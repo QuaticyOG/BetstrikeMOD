e4e0109a2f764fa2e883eb9f245dde473029967b
QuaticyOG <89028089+QuaticyOG@users.noreply.github.com>
2026-03-09 14:41:29 +0100

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "messageCreate",
+  async execute(message) {
+
+    // Ignore bots
+    if (message.author.bot) return;
+
+    // You can add message-based features here later
+
+  }
+};
