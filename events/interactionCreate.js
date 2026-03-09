4b0d41600a8004196b9e828f4e0456c22b5f0da4
QuaticyOG <89028089+QuaticyOG@users.noreply.github.com>
2026-03-09 14:09:52 +0100

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -39,7 +39,25 @@ module.exports = {
   name: Events.InteractionCreate,
 
   async execute(interaction) {
+    // SLASH COMMAND HANDLER
+if (interaction.isChatInputCommand()) {
 
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+
+    await interaction.reply({
+      content: "There was an error executing this command.",
+      ephemeral: true
+    });
+  }
+
+}
     // BUTTONS
     if (interaction.isButton()) {
 
