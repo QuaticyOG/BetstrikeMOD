72f1fc7381debaaaa21a46a275b99ae96f3a6dfa
QuaticyOG <89028089+QuaticyOG@users.noreply.github.com>
2026-03-09 14:39:58 +0100

    Update eventHandler.js

--- systems/eventHandler.js
+++ systems/eventHandler.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-const { logger } = require('../utils/logger');
+const fs = require("fs");
+const path = require("path");
+const { logger } = require("../utils/logger");
 
 async function loadEvents(client) {
-  const eventsPath = path.join(__dirname, '..', 'events');
-  const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+  const eventsPath = path.join(__dirname, "..", "events");
+  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
 
   for (const file of eventFiles) {
+
     const event = require(path.join(eventsPath, file));
+
     if (!event.name || !event.execute) {
       logger.warn(`Skipping invalid event file: ${file}`);
       continue;
     }
 
-    if (event.once) client.once(event.name, (...args) => event.execute(...args));
-    else client.on(event.name, (...args) => event.execute(...args));
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
+
   }
 
   logger.info(`Loaded ${eventFiles.length} events`);
