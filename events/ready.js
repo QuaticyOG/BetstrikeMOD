ab3903a8bbcd8d64d944fab7b096d65acc270644
QuaticyOG <89028089+QuaticyOG@users.noreply.github.com>
2026-03-09 15:21:09 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 const { logger } = require('../utils/logger');
 
 module.exports = {
-  name: 'ready',
+name: "clientReady",
   once: true,
   async execute(client) {
     logger.info(`Logged in as ${client.user.tag}`);
